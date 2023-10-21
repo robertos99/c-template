2c9eaec94e0b0f1606241efd52951aa21b519f32
Robert <games.robert2@t-online.de>
2023-10-21 10:35:28 +0200

    gitignore things and helloworld

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+const char *say_hello()
+{
+    return "Hello, World!";
+}
+
+int main()
+{
+    puts(say_hello());
+    return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+const char *say_hello();
+
+#endif
